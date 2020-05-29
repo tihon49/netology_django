6efec9f1d8db77d0a01f4e287c63c8e77dbb3f08
Tihon49 <tihon49@gmail.com>
2020-05-29 15:38:55 +0300

    first task done

--- 2. Обработка запросов/landing/app/urls.py
+++ 2. Обработка запросов/landing/app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
-from app.views import landing, stats, index
+from app.views import landing, stats, index, landing_alternate
 
 
 urlpatterns = [
     path('', index, name='index'),
     path('landing/', landing, name='landing'),
+    path('landing_alternate/' , landing_alternate, name='landing_alternate'),
     path('stats/', stats, name='stats'),
 ]
--- 2. Обработка запросов/landing/app/views.py
+++ 2. Обработка запросов/landing/app/views.py
@@ -1,5 +1,4 @@
 from collections import Counter
-
 from django.shortcuts import render_to_response
 
 # Для отладки механизма ab-тестирования используйте эти счетчики
@@ -12,6 +11,12 @@ counter_click = Counter()
 
 def index(request):
     # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
+    if request.GET.get('from-landing') == 'original':
+        counter_click['original'] += 1
+        print(counter_click)
+    elif request.GET.get('from-landing') == 'test':
+        counter_click['test'] += 1
+        print(counter_click)
     return render_to_response('index.html')
 
 
@@ -20,7 +25,15 @@ def landing(request):
     # в зависимости от GET параметра ab-test-arg
     # который может принимать значения original и test
     # Так же реализуйте логику подсчета количества показов
+    # if request.GET.get('landing'):
+    counter_show['original'] += 1
+    print(counter_show)
     return render_to_response('landing.html')
+
+def landing_alternate(request):
+    counter_show['test'] += 1
+    print(counter_show)
+    return render_to_response('landing_alternate.html')
 
 
 def stats(request):
@@ -28,7 +41,17 @@ def stats(request):
     # Чтобы отличить с какой версии лендинга был переход
     # проверяйте GET параметр marker который может принимать значения test и original
     # Для вывода результат передайте в следующем формате:
+    try:
+        test = counter_click['test'] / counter_show['test']
+    except ZeroDivisionError:
+        test = 0
+
+    try:
+        original = counter_click['original'] / counter_show['original']
+    except ZeroDivisionError:
+        original = 0
+
     return render_to_response('stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+        'test_conversion': round(test, 2),
+        'original_conversion': round(original, 2),
     })
