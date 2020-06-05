d8295ada9f3be735c020d7c388dc2f3dbca945dc
Tihon49 <tihon49@gmail.com>
2020-06-05 16:23:17 +0300

    first_task digit check fail

--- 3. Динамческое формирование страниц на основе швблонов/task1/app/views.py
+++ 3. Динамческое формирование страниц на основе швблонов/task1/app/views.py
@@ -1,6 +1,26 @@
 from pprint import pprint
-
+import csv
 from django.shortcuts import render
+
+
+
+def str_to_float(lst):
+    '''
+    :param lst: get a list
+    :return: str items => float items
+    '''
+    lists_list = []
+    for i in lst:
+        dig_list = []
+        for j in i:
+            try:
+                if j > 1000:
+                    j = float(j)
+            except:
+                pass
+            dig_list.append(j)
+        lists_list.append(dig_list)
+    return lists_list
 
 
 
@@ -10,14 +30,11 @@ def inflation_view(request):
     # чтение csv-файла и заполнение контекста
     context = {}
     with open('inflation_russia.csv', encoding='utf-8') as csv_file:
-        data = csv_file.readlines()
-        title = data[0]
-        title = title.split(";")
-        body = data[1:]
-        body = [i.split(';') for i in body]
+        reader = csv.reader(csv_file, delimiter=';')
+        data = str_to_float(reader)
 
-        context['title'] = title
-        context['body'] = body
+    context['title'] = data[0]
+    context['body'] = data[1:]
 
     return render(request, f'app/{template_name}',
                   context)
