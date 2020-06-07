ef6691d5b09e6822691732b1c896cb07a14e94f3
Tihon49 <tihon49@gmail.com>
2020-06-07 18:51:43 +0300

    3-d task done

--- 3. Динамческое формирование страниц на основе швблонов/task3/app/templatetags/news_filters.py
+++ 3. Динамческое формирование страниц на основе швблонов/task3/app/templatetags/news_filters.py
@@ -1,3 +1,4 @@
+import time
 from django import template
 
 
@@ -7,16 +8,46 @@ register = template.Library()
 @register.filter
 def format_date(value):
     # Ваш код
-    return value
+    now = time.time()
+    value = float(value)
+    if (now - value) / 60 < 10:
+        return 'только что'
+    elif (now - value) / 60 / 60 < 24:
+        return f'{int((now - value) / 60 / 60)} часов назад'
+    elif (now - value) / 60 / 60 > 24:
+        return time.ctime(value)
+
 
 
 # необходимо добавить фильтр для поля `score`
+@register.filter
+def score_filter(value):
+    if value < -5:
+        return 'все плохо'
+    elif -5 < value < 5:
+        return 'нейтрально'
+    elif value > 5:
+        return 'хорошо'
+    else:
+        return 'нет рейтинга'
+
 
 
 @register.filter
 def format_num_comments(value):
     # Ваш код
-    return value
+    if value == 0:
+        return 'оставьте комментарий'
+    elif 0 < value < 50:
+        return value
+    elif value > 50:
+        return '50+'
 
 
 
+@register.filter
+def format_selftext(value, num=3):
+    if len(value.split()) > num:
+        return f'{" ".join(value.split()[:num])} ... {" ".join(value.split()[-num:])}'
+    else:
+        return value
