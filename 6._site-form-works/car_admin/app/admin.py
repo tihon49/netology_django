16ad0cedd336676f1d5bf7f6ad0f83a196452e83
Tihon49 <tihon49@gmail.com>
2020-06-21 20:27:11 +0300

    first task in progress

--- 6._site-form-works/car_admin/app/admin.py
+++ 6._site-form-works/car_admin/app/admin.py
@@ -7,6 +7,7 @@ from .forms import ReviewAdminForm
 class CarAdmin(admin.ModelAdmin):
     list_display = ('brand', 'model', 'review_count')
     list_filter = ('brand', 'id', 'model')
+    search_fields = ['brand', 'model']
 
 
 class ReviewAdmin(admin.ModelAdmin):
